5352f73ae59393c479568240ccd9a2644e1d693e
Elliott Eggleston <ejegg@ejegg.com>
2016-02-18 01:28:02 -0500

    Accept uploaded fractal images

--- fractals/views.py
+++ fractals/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.template import Context, loader
+from django.conf import settings
 from models import Fractal
 from django.views.decorators.csrf import csrf_exempt
 import logging
+import re
 
 def index(request, id = -1):
     template = loader.get_template('fractals/index.html')
@@ -33,27 +35,42 @@ def save(request, id = None):
     logger = logging.getLogger('django')
     name = request.POST.get('name', '')
     serializedTransforms = request.POST.get('serializedTransforms', '')
-    logger.debug('Name is {0}, transforms are {1}'.format(name, serializedTransforms))
+    uploaded = request.FILES['thumbnail']
+    logger.debug('Name is {0}, transforms are {1}, thumb name is {2}'.format(name, serializedTransforms, uploaded.name))
+
+    if re.match('frac_-?[0-9]+.png$', uploaded.name) == None:
+        logger.debug('Illegal thumbnail file name')
+        return HttpResponse("Error: invalid thumbnail file name")
+
     if name == '' or serializedTransforms == '':
         return HttpResponse("Error: blank name or transforms")
-    
+
     transformParts = serializedTransforms.split(' ');
     if len(transformParts) % 16 != 0:
         return HttpResponse("Error: transform count must be a multiple of 16")
-    
-    numTransforms = len(transformParts) / 16 
-    
-    fractal = Fractal(
-                      id = id,
-                      name= name,
-                      transformCount = numTransforms, 
-                      transforms = serializedTransforms,
-                      thumbnailWidth = 0,
-                      thumbnailHeight = 0
-                      )
+
+    numTransforms = len(transformParts) / 16
+    logger.debug('Passed initial checks, will try to save')
     try:
+        filename = settings.MEDIA_ROOT + uploaded.name
+        logger.debug('Trying to write thumbnail to {0}'.format(filename))
+        with open(filename, 'w') as thumbfile:
+            for chunk in uploaded.chunks():
+                thumbfile.write(chunk)
+
+        logger.debug('Wrote thumbnail, saving fractal to database')
+        fractal = Fractal(
+                          id = id,
+                          name= name,
+                          transformCount = numTransforms,
+                          transforms = serializedTransforms,
+                          thumbnailWidth = 0,
+                          thumbnailHeight = 0,
+                          thumbnail = uploaded.name
+                          )
         fractal.save()
+        logger.debug('Saved fractal to db')
     except Exception as e:
         logger.exception(e)
-    
+
     return HttpResponse("Saved!")
