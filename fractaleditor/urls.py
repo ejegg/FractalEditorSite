6a56f276d15c9ae93fd99aff827974e00e171028
Elliott Eggleston <ejegg@ejegg.com>
2016-02-19 01:10:05 -0500

    Add home page and app download link
    Root URL is no longer an error page!

--- fractaleditor/urls.py
+++ fractaleditor/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'fractaleditor.views.home', name='home'),
     # url(r'^fractaleditor/', include('fractaleditor.foo.urls')),
     url(r'^fractal/', include('fractals.urls')),
+    url(r'^$', 'fractaleditor.views.home', name='home'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
---
+++ fractaleditor/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.template import Context, loader
+from fractals.models import Fractal
+
+def home(request, id = -1):
+    template = loader.get_template('home.html')
+
+    allFrac = Fractal.objects.all()
+    for fractal in allFrac:
+        if fractal.thumbnail == '':
+            fractal.hasthumbnail = False
+        else:
+            fractal.hasthumbnail = True
+    context = Context({
+        'fractals' : allFrac
+    })
+    return HttpResponse(template.render(context))
