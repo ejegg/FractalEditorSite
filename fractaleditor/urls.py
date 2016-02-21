0b2f9034443ffec929038994e4dee631f80cfe9d
Elliott Eggleston <ejegg@ejegg.com>
2016-02-21 00:12:35 -0500

    Add a favicon
    Wow, these got complicated.  Thank you realfavicongenerator.net

--- fractaleditor/urls.py
+++ fractaleditor/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
+import os.path
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
-
+project_root = os.path.dirname(os.path.dirname(__file__))
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'fractaleditor.views.home', name='home'),
@@ -11,6 +12,12 @@ urlpatterns = patterns('',
     url(r'^fractal/', include('fractals.urls')),
     url(r'^$', 'fractaleditor.views.home', name='home'),
     url(r'^app$', 'fractals.views.app_link'),
+    url(r'^(?P<path>android-chrome-[0-9x]+\.png|apple-touch-icon-[0-9a-z]+\.png|' +
+            r'browserconfig.xml|favicon-[0-9x]+\.png|favicon.ico|manifest.json|' +
+            r'safari-pinned-tab.svg|mstile-[0-9x]+.png)$',
+        'django.views.static.serve', {
+            'document_root': project_root + '/static'
+        }),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
