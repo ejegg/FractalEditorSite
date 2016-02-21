b80eabaeb3010d471d5570b983a3f55069467df9
Elliott Eggleston <ejegg@ejegg.com>
2016-02-20 19:35:22 -0500

    Sort newest fractals first

--- fractaleditor/views.py
+++ fractaleditor/views.py
@@ -5,7 +5,7 @@ from fractals.models import Fractal
 def home(request, id = -1):
     template = loader.get_template('home.html')
 
-    allFrac = Fractal.objects.all()
+    allFrac = Fractal.objects.all().order_by('-id')
     for fractal in allFrac:
         if fractal.thumbnail == '':
             fractal.hasthumbnail = False
