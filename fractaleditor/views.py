a7562a7796ea0c220b0389c1cb4c8cfd458425c1
Elliott Eggleston <ejegg@ejegg.com>
2016-04-02 21:23:05 -0400

    Move show_edit_links to request processor
    Share it between all templates

--- fractaleditor/views.py
+++ fractaleditor/views.py
@@ -1,22 +1,17 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from fractals.models import Fractal
-import re
 
 def home(request, id = -1):
     template = loader.get_template('home.html')
 
     allFrac = Fractal.objects.all().order_by('-id')
-    is_android = False
-    if re.match('.*Android.*', request.META['HTTP_USER_AGENT']) is not None:
-        is_android = True
     for fractal in allFrac:
         if fractal.thumbnail == '':
             fractal.hasthumbnail = False
         else:
             fractal.hasthumbnail = True
     context = RequestContext(request, {
-        'fractals' : allFrac,
-        'show_edit_links' : is_android
+        'fractals' : allFrac
     })
     return HttpResponse(template.render(context))
