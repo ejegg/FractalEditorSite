6017c5063f9b42f5caa82ac180a28650cef2715c
Elliott Eggleston <ejegg@ejegg.com>
2017-11-10 14:01:21 -0500

    Update urls to work with django 1.10

--- fractaleditor/urls.py
+++ fractaleditor/urls.py
@@ -1,21 +1,21 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
+import django.views.static
+import fractaleditor.views
+import fractals.views
 import os.path
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 project_root = os.path.dirname(os.path.dirname(__file__))
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'fractaleditor.views.home', name='home'),
-    # url(r'^fractaleditor/', include('fractaleditor.foo.urls')),
+urlpatterns = [
     url(r'^fractal/', include('fractals.urls')),
-    url(r'^$', 'fractaleditor.views.home', name='home'),
-    url(r'^app$', 'fractals.views.app_link'),
+    url(r'^$', fractaleditor.views.home, name='home'),
+    url(r'^app$', fractals.views.app_link),
     url(r'^(?P<path>android-chrome-[0-9x]+\.png|apple-touch-icon-[0-9a-z]+\.png|' +
             r'browserconfig.xml|favicon-[0-9x]+\.png|favicon.ico|manifest.json|' +
             r'safari-pinned-tab.svg|mstile-[0-9x]+.png|.well-known/.*)$',
-        'django.views.static.serve', {
+        django.views.static.serve, {
             'document_root': project_root + '/static'
         }),
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -23,4 +23,4 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-)
+]
--- fractals/urls.py
+++ fractals/urls.py
@@ -1,16 +1,12 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from fractals import views
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'fractaleditor.views.home', name='home'),
-    # url(r'^fractaleditor/', include('fractaleditor.foo.urls')),
-    
+urlpatterns = [
     url(r'save', views.save),
     url(r'^$',views.index),
     url(r'^(?P<id>\d+)$', views.index)
-)
+]
